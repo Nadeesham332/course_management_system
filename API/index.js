bc58faf9abf6fb062359fbc7b0d72accad4f0336
pradeepdharmasena <39731829+pradeepdharmasena@users.noreply.github.com>
2022-07-11 20:09:32 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -29,48 +29,4 @@ app.get('/courseData', async (req, res)=>{
   res.send(results);
 })
 
-/*
-app.post('/signup', async (req, res)=>{
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const contactNo = req.body.contactNo;
-  const birthday = req.body.birthday;
-  const disrict_id = req.body.district;
-  const address = req.body.address;
-  const gender_name = req.body.gender;
-  const password = req.body.password;
-  let hashedpw = "null"
-  try{
-      hashedpw = await hash(password, 10);
-      console.log("encripted password" , hashedpw)
-  }catch{
-      res.send({status:"password encription failed", error:"true"})
-  }
-  var gender = "1";
-  if(gender_name ==="Female" || gender_name ==="female"){
-      gender = "2";
-  }
-  const today = todayDate();
-  const quary2 = "CALL insert_users(?,?,?,?,?,?,?,?,?,?,?,?)"
-  var values = values = [email,contactNo, hashedpw,"1", firstName, lastName, gender , "not added", disrict_id , address, birthday , today ]
-  console.log(values)
-
-  db.query(
-      quary2,
-      values,
-      (err, results)=>{
-          if(err){
-              console.log("Error occured", err)
-              res.send({isSucsess:false})
-          }else{
-              console.log("Values inserted")
-              res.send({isSucsess:true})
-          }
-      }
-  );
-
-});
-*/
-
 app.listen(3000 , ()=> console.log("Listening on port 3000...."))
