0e87205c752344aaf7406c32c9a7e28f40ae1a2a
Nadeesha Maduranga <nadeesham332@gmail.com>
2022-07-27 13:55:59 +0530

    navbar mobile view

--- frontend-cms/src/App.js
+++ frontend-cms/src/App.js
@@ -1,7 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navigation/Navbar.js";
+
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
     </div>
   );
 }
